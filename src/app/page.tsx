03b42b6451697b1067dc66402864e35a007e1b64
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-10-26 23:21:47 +0530

    cta section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import { Hero } from "./landing/components/sections/Hero";
 import Navbar from "./landing/components/sections/Navbar";
 import { Cta } from "./landing/components/sections/Cta";
 import { Footer } from "./landing/components/sections/Footer";
+import { Features } from "./landing/components/sections/Features";
+import { About } from "./landing/components/sections/About";
 
 export default function Page() {
   return (
     <main className="min-h-screen bg-black ">
       <Navbar />
       <Hero />
+      <About />
+      <Features />
       <Cta />
       <Footer />
     </main>
